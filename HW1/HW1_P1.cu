8e7c70ae5c9797d30aabafcdf2725309b4642541
Huy Nguyễn <hfake.xyz@gmail.com>
2024-04-23 12:49:25 +0700

    Done HW1

---
+++ HW1/HW1_P1.cu
@@ -0,0 +1,255 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define CHECK(call)\
+{\
+	const cudaError_t error = call;\
+	if (error != cudaSuccess)\
+	{\
+		fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+		fprintf(stderr, "code: %d, reason: %s\n", error,\
+				cudaGetErrorString(error));\
+		exit(EXIT_FAILURE);\
+	}\
+}
+
+struct GpuTimer
+{
+	cudaEvent_t start;
+	cudaEvent_t stop;
+
+	GpuTimer()
+	{
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+	}
+
+	~GpuTimer()
+	{
+		cudaEventDestroy(start);
+		cudaEventDestroy(stop);
+	}
+
+	void Start()
+	{
+		cudaEventRecord(start, 0);                                                                 
+		cudaEventSynchronize(start);
+	}
+
+	void Stop()
+	{
+		cudaEventRecord(stop, 0);
+	}
+
+	float Elapsed()
+	{
+		float elapsed;
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&elapsed, start, stop);
+		return elapsed;
+	}
+};
+
+void readPnm(char * fileName, 
+		int &numChannels, int &width, int &height, uint8_t * &pixels)
+{
+	FILE * f = fopen(fileName, "r");
+	if (f == NULL)
+	{
+		printf("Cannot read %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	char type[3];
+	fscanf(f, "%s", type);
+	if (strcmp(type, "P2") == 0)
+		numChannels = 1;
+	else if (strcmp(type, "P3") == 0)
+		numChannels = 3;
+	else // In this exercise, we don't touch other types
+	{
+		fclose(f);
+		printf("Cannot read %s\n", fileName); 
+		exit(EXIT_FAILURE); 
+	}
+
+	fscanf(f, "%i", &width);
+	fscanf(f, "%i", &height);
+
+	int max_val;
+	fscanf(f, "%i", &max_val);
+	if (max_val > 255) // In this exercise, we assume 1 byte per value
+	{
+		fclose(f);
+		printf("Cannot read %s\n", fileName); 
+		exit(EXIT_FAILURE); 
+	}
+
+	pixels = (uint8_t *)malloc(width * height * numChannels);
+	for (int i = 0; i < width * height * numChannels; i++)
+		fscanf(f, "%hhu", &pixels[i]);
+
+	fclose(f);
+}
+
+void writePnm(uint8_t * pixels, int numChannels, int width, int height, 
+		char * fileName)
+{
+	FILE * f = fopen(fileName, "w");
+	if (f == NULL)
+	{
+		printf("Cannot write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}	
+
+	if (numChannels == 1)
+		fprintf(f, "P2\n");
+	else if (numChannels == 3)
+		fprintf(f, "P3\n");
+	else
+	{
+		fclose(f);
+		printf("Cannot write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(f, "%i\n%i\n255\n", width, height); 
+
+	for (int i = 0; i < width * height * numChannels; i++)
+		fprintf(f, "%hhu\n", pixels[i]);
+
+	fclose(f);
+}
+
+__global__ void convertRgb2GrayKernel(uint8_t* inPixels, int width, int height, uint8_t* outPixels) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idy = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (idx < width && idy < height) {
+        int i = idy * width + idx;
+        uint8_t red = inPixels[3 * i];
+        uint8_t green = inPixels[3 * i + 1];
+        uint8_t blue = inPixels[3 * i + 2];
+        outPixels[i] = 0.299f * red + 0.587f * green + 0.114f * blue;
+    }
+}
+
+void convertRgb2Gray(uint8_t * inPixels, int width, int height,
+		uint8_t * outPixels, 
+		bool useDevice=false, dim3 blockSize=dim3(1))
+{
+	GpuTimer timer;
+	timer.Start();
+	if (useDevice == false)
+	{
+        // Reminder: gray = 0.299*red + 0.587*green + 0.114*blue  
+        for (int r = 0; r < height; r++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+                int i = r * width + c;
+                uint8_t red = inPixels[3 * i];
+                uint8_t green = inPixels[3 * i + 1];
+                uint8_t blue = inPixels[3 * i + 2];
+                outPixels[i] = 0.299f*red + 0.587f*green + 0.114f*blue;
+            }
+        }
+	}
+    else // use device 
+	{
+        cudaDeviceProp devProp;
+		cudaGetDeviceProperties(&devProp, 0);
+		printf("GPU name: %s\n", devProp.name);
+		printf("GPU compute capability: %d.%d\n", devProp.major, devProp.minor);
+        CHECK(cudaGetDeviceProperties(&devProp, 0));
+
+        // Allocate device memories
+        uint8_t* d_inPixels;
+        uint8_t* d_outPixels;
+        size_t size = width * height * sizeof(uint8_t) * 3; // Assuming 3 channels for RGB
+
+        CHECK(cudaMalloc(&d_inPixels, size));
+        CHECK(cudaMalloc(&d_outPixels, width * height * sizeof(uint8_t)));
+
+        // Copy data to device memories
+        CHECK(cudaMemcpy(d_inPixels, inPixels, size, cudaMemcpyHostToDevice));
+
+        // Set grid size and call kernel
+        dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+        convertRgb2GrayKernel<<<gridSize, blockSize>>>(d_inPixels, width, height, d_outPixels);
+        CHECK(cudaDeviceSynchronize());
+        CHECK(cudaGetLastError());
+
+        // Copy result from device memories
+        CHECK(cudaMemcpy(outPixels, d_outPixels, width * height * sizeof(uint8_t), cudaMemcpyDeviceToHost));
+
+        // Free device memories
+        CHECK(cudaFree(d_inPixels));
+        CHECK(cudaFree(d_outPixels));
+    }
+
+    timer.Stop();
+    float time = timer.Elapsed();
+    printf("Processing time (%s): %f ms\n\n", useDevice ? "use device" : "use host", time);
+}
+
+float computeError(uint8_t * a1, uint8_t * a2, int n)
+{
+	float err = 0;
+	for (int i = 0; i < n; i++)
+		err += abs((int)a1[i] - (int)a2[i]);
+	err /= n;
+	return err;
+}
+
+char * concatStr(const char * s1, const char * s2)
+{
+	char * result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+	strcpy(result, s1);
+	strcat(result, s2);
+	return result;
+}
+
+int main(int argc, char ** argv)
+{	
+	if (argc != 3 && argc != 5)
+	{
+		printf("The number of arguments is invalid\n");
+		return EXIT_FAILURE;
+	}
+
+	// Read input RGB image file
+	int numChannels, width, height;
+	uint8_t * inPixels;
+	readPnm(argv[1], numChannels, width, height, inPixels);
+	if (numChannels != 3)
+		return EXIT_FAILURE; // Input image must be RGB
+	printf("Image size (width x height): %i x %i\n\n", width, height);
+
+	// Convert RGB to grayscale not using device
+	uint8_t * correctOutPixels= (uint8_t *)malloc(width * height);
+	convertRgb2Gray(inPixels, width, height, correctOutPixels);
+
+	// Convert RGB to grayscale using device
+	uint8_t * outPixels= (uint8_t *)malloc(width * height);
+	dim3 blockSize(32, 32); // Default
+	if (argc == 5)
+	{
+		blockSize.x = atoi(argv[3]);
+		blockSize.y = atoi(argv[4]);
+	} 
+	convertRgb2Gray(inPixels, width, height, outPixels, true, blockSize); 
+
+	// Compute mean absolute error between host result and device result
+	float err = computeError(outPixels, correctOutPixels, width * height);
+	printf("Error between device result and host result: %f\n", err);
+
+	// Write results to files
+	char * outFileNameBase = strtok(argv[2], "."); // Get rid of extension
+	writePnm(correctOutPixels, 1, width, height, concatStr(outFileNameBase, "_host.pnm"));
+	writePnm(outPixels, 1, width, height, concatStr(outFileNameBase, "_device.pnm"));
+
+	// Free memories
+	free(inPixels);
+	free(outPixels);
+}
---
+++ HW1/HW1_P2.cu
@@ -0,0 +1,300 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define CHECK(call)\
+{\
+	const cudaError_t error = call;\
+	if (error != cudaSuccess)\
+	{\
+		fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+		fprintf(stderr, "code: %d, reason: %s\n", error,\
+				cudaGetErrorString(error));\
+		exit(EXIT_FAILURE);\
+	}\
+}
+
+struct GpuTimer
+{
+    cudaEvent_t start;
+    cudaEvent_t stop;
+
+    GpuTimer()
+    {
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+    }
+
+    ~GpuTimer()
+    {
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    void Start()
+    {
+        cudaEventRecord(start, 0);
+    }
+
+    void Stop()
+    {
+        cudaEventRecord(stop, 0);
+    }
+
+    float Elapsed()
+    {
+        float elapsed;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+        return elapsed;
+    }
+};
+
+void readPnm(char * fileName, 
+		int &width, int &height, uchar3 * &pixels)
+{
+	FILE * f = fopen(fileName, "r");
+	if (f == NULL)
+	{
+		printf("Cannot read %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	char type[3];
+	fscanf(f, "%s", type);
+	
+	if (strcmp(type, "P3") != 0) // In this exercise, we don't touch other types
+	{
+		fclose(f);
+		printf("Cannot read %s\n", fileName); 
+		exit(EXIT_FAILURE); 
+	}
+
+	fscanf(f, "%i", &width);
+	fscanf(f, "%i", &height);
+	
+	int max_val;
+	fscanf(f, "%i", &max_val);
+	if (max_val > 255) // In this exercise, we assume 1 byte per value
+	{
+		fclose(f);
+		printf("Cannot read %s\n", fileName); 
+		exit(EXIT_FAILURE); 
+	}
+
+	pixels = (uchar3 *)malloc(width * height * sizeof(uchar3));
+	for (int i = 0; i < width * height; i++)
+		fscanf(f, "%hhu%hhu%hhu", &pixels[i].x, &pixels[i].y, &pixels[i].z);
+
+	fclose(f);
+}
+
+void writePnm(uchar3 * pixels, int width, int height, 
+		char * fileName)
+{
+	FILE * f = fopen(fileName, "w");
+	if (f == NULL)
+	{
+		printf("Cannot write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}	
+
+	fprintf(f, "P3\n%i\n%i\n255\n", width, height); 
+
+	for (int i = 0; i < width * height; i++)
+		fprintf(f, "%hhu\n%hhu\n%hhu\n", pixels[i].x, pixels[i].y, pixels[i].z);
+	
+	fclose(f);
+}
+
+__global__ void blurImgKernel(uchar3 * inPixels, int width, int height, 
+		float * filter, int filterWidth, 
+		uchar3 * outPixels)
+{
+    int r = blockIdx.y * blockDim.y + threadIdx.y;
+    int c = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (r < height && c < width) {
+        float3 result = make_float3(0.0f, 0.0f, 0.0f);
+
+        int half = filterWidth / 2;
+
+        for (int i = -half; i <= half; ++i) {
+            for (int j = -half; j <= half; ++j) {
+                int currR = r + i;
+                int currC = c + j;
+
+                currR = max(0, min(currR, height - 1));
+                currC = max(0, min(currC, width - 1));
+
+                float filterValue = filter[(i + half) * filterWidth + (j + half)];
+                uchar3 pixel = inPixels[currR * width + currC];
+                result.x += filterValue * pixel.x;
+                result.y += filterValue * pixel.y;
+                result.z += filterValue * pixel.z;
+            }
+        }
+
+        outPixels[r * width + c] = make_uchar3((unsigned char)result.x, (unsigned char)result.y, (unsigned char)result.z);
+    }
+	
+}
+
+void blurImg(uchar3 * inPixels, int width, int height, float * filter, int filterWidth, 
+		uchar3 * outPixels,
+		bool useDevice=false, dim3 blockSize=dim3(1, 1))
+{
+	GpuTimer timer;
+	timer.Start();
+	if (useDevice == false)
+	{
+        for (int r = 0; r < height; ++r) {
+            for (int c = 0; c < width; ++c) {
+                float3 result = make_float3(0.0f, 0.0f, 0.0f);
+
+                int half = filterWidth / 2;
+
+                for (int i = -half; i <= half; ++i) {
+                    for (int j = -half; j <= half; ++j) {
+                        int currR = r + i;
+                        int currC = c + j;
+
+                        currR = max(0, min(currR, height - 1));
+                        currC = max(0, min(currC, width - 1));
+
+                        float filterValue = filter[(i + half) * filterWidth + (j + half)];
+                        uchar3 pixel = inPixels[currR * width + currC];
+                        result.x += filterValue * pixel.x;
+                        result.y += filterValue * pixel.y;
+                        result.z += filterValue * pixel.z;
+                    }
+                }
+
+                outPixels[r * width + c] = make_uchar3((unsigned char)result.x, (unsigned char)result.y, (unsigned char)result.z);
+            }
+        }
+
+	}
+	else // Use device
+	{
+		cudaDeviceProp devProp;
+		cudaGetDeviceProperties(&devProp, 0);
+		printf("GPU name: %s\n", devProp.name);
+		printf("GPU compute capability: %d.%d\n", devProp.major, devProp.minor);
+
+		uchar3 *d_inPixels, *d_outPixels;
+        float *d_filter;
+
+        cudaMalloc((void **)&d_inPixels, width * height * sizeof(uchar3));
+        cudaMalloc((void **)&d_outPixels, width * height * sizeof(uchar3));
+        cudaMalloc((void **)&d_filter, filterWidth * filterWidth * sizeof(float));
+        
+        cudaMemcpy(d_inPixels, inPixels, width * height * sizeof(uchar3), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_filter, filter, filterWidth * filterWidth * sizeof(float), cudaMemcpyHostToDevice);
+
+        dim3 grid((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+        blurImgKernel<<<grid, blockSize>>>(d_inPixels, width, height, d_filter, filterWidth, d_outPixels);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(outPixels, d_outPixels, width * height * sizeof(uchar3), cudaMemcpyDeviceToHost);
+
+        cudaFree(d_inPixels);
+        cudaFree(d_outPixels);
+        cudaFree(d_filter);
+
+	}
+	timer.Stop();
+	float time = timer.Elapsed();
+	printf("Processing time (%s): %f ms\n", 
+    		useDevice == true? "use device" : "use host", time);
+}
+
+float computeError(uchar3 * a1, uchar3 * a2, int n)
+{
+	float err = 0;
+	for (int i = 0; i < n; i++)
+	{
+		err += abs((int)a1[i].x - (int)a2[i].x);
+		err += abs((int)a1[i].y - (int)a2[i].y);
+		err += abs((int)a1[i].z - (int)a2[i].z);
+	}
+	err /= (n * 3);
+	return err;
+}
+
+char * concatStr(const char * s1, const char * s2)
+{
+    char * result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+int main(int argc, char ** argv)
+{
+	if (argc != 4 && argc != 6)
+	{
+		printf("The number of arguments is invalid\n");
+		return EXIT_FAILURE;
+	}
+
+	// Read input image file
+	int width, height;
+	uchar3 * inPixels;
+	readPnm(argv[1], width, height, inPixels);
+	printf("Image size (width x height): %i x %i\n\n", width, height);
+
+	// Read correct output image file
+	int correctWidth, correctHeight;
+	uchar3 * correctOutPixels;
+	readPnm(argv[3], correctWidth, correctHeight, correctOutPixels);
+	if (correctWidth != width || correctHeight != height)
+	{
+		printf("The shape of the correct output image is invalid\n");
+		return EXIT_FAILURE;
+	}
+
+	// Set up a simple filter with blurring effect 
+	int filterWidth = 9;
+	float * filter = (float *)malloc(filterWidth * filterWidth * sizeof(float));
+	for (int filterR = 0; filterR < filterWidth; filterR++)
+	{
+		for (int filterC = 0; filterC < filterWidth; filterC++)
+		{
+			filter[filterR * filterWidth + filterC] = 1. / (filterWidth * filterWidth);
+		}
+	}
+
+	// Blur input image using host
+	uchar3 * hostOutPixels = (uchar3 *)malloc(width * height * sizeof(uchar3)); 
+	blurImg(inPixels, width, height, filter, filterWidth, hostOutPixels);
+	
+	// Compute mean absolute error between host result and correct result
+	float hostErr = computeError(hostOutPixels, correctOutPixels, width * height);
+	printf("Error: %f\n\n", hostErr);
+
+	// Blur input image using device
+	uchar3 * deviceOutPixels = (uchar3 *)malloc(width * height * sizeof(uchar3));
+	dim3 blockSize(32, 32); // Default
+	if (argc == 6)
+	{
+		blockSize.x = atoi(argv[4]);
+		blockSize.y = atoi(argv[5]);
+	}  
+	blurImg(inPixels, width, height, filter, filterWidth, deviceOutPixels, true, blockSize);
+
+	// Compute mean absolute error between device result and correct result
+	float deviceErr = computeError(deviceOutPixels, correctOutPixels, width * height);
+	printf("Error: %f\n\n", deviceErr);
+
+	// Write results to files
+	char * outFileNameBase = strtok(argv[2], "."); // Get rid of extension
+	writePnm(hostOutPixels, width, height, concatStr(outFileNameBase, "_host.pnm"));
+	writePnm(deviceOutPixels, width, height, concatStr(outFileNameBase, "_device.pnm"));
+
+	// Free memories
+	free(inPixels);
+	free(correctOutPixels);
+	free(hostOutPixels);
+	free(deviceOutPixels);
+	free(filter);
+}
